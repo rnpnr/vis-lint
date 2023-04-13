40195720d82f2e4ce91a3deab0ed68756815cd20
Randy Palamar <palamar@ualberta.ca>
2023-04-13 11:40:39 -0600

    initial commit
    code from a comment I left in https://github.com/martanne/vis/issues/1070

---
+++ README.md
@@ -0,0 +1,14 @@
+# vis-lint
+
+Lint the currently open file in [Vis](https://github.com/martanne/vis)
+and display the output in the message window.
+
+# Installation
+
+See [Plugins](https://github.com/martanne/vis/wiki/Plugins) on the
+Vis wiki.
+
+# Usage
+Type the following into the prompt:
+
+	:lint
---
+++ vis-lint.lua
@@ -0,0 +1,19 @@
+vis:command_register("lint", function()
+	local linters = {}
+	linters["bash"] = "shellcheck"
+
+	local cmd = linters[vis.win.syntax]
+	if cmd == nil then
+		vis:info(vis.win.syntax .. ": linter not defined")
+		return false
+	end
+
+	local file = vis.win.file
+	local _, ostr, estr = vis:pipe(file, {start = 0, finish = 0}, cmd .. " " .. file.name)
+	if estr then
+		vis:message(estr)
+		return false
+	end
+	vis:message(ostr)
+	return true
+end, "Lint the current file and display output in the message window")
